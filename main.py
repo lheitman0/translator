a52cbe209d6182acda2e12126d114e827e904291
Luke Heitman <lheitman00@gmail.com>
2023-11-29 00:55:45 -0500

    end commit, need to fix

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import ffmpeg
 import requests
 import json
 import os
+import numpy as np
 import openai
 from openai import OpenAI
-import keyboard
+from pynput import keyboard
 from pydub import AudioSegment
 from pydub.playback import play
 
@@ -23,38 +24,65 @@ client = OpenAI()
 chat_history = []
 output_file_path = "output.wav"
 
-print("\n# Welcome to your AI-powered voice translator #\n")
-
-output_language = input("Please enter desired output language: ")
+# output_language = input("Please enter desired output language: ")
 # Function definitions
 
-def start_recording():
-    print("Recording started...")
-    recording = sd.rec(samplerate=44100, channels=2, blocking=False)
-    return recording
-
+def start_recording(duration=5):
+    try:
+        print("Recording for {} seconds...".format(duration))
+        recording = sd.rec(int(duration * 44100), samplerate=44100, channels=1)
+        sd.wait()
+        return recording
+    except Exception as e:
+        print(f"An error occurred during recording: {e} ")
+        return None
 
 def stop_recording_and_process(recording):
-    sd.stop()
-    print("Recording stopped, processing audio...\n")
-    write_file(recording, output_file_path)
-    transcribe()
+    try:
+        sd.stop()
+        print("Recording stopped, processing audio...\n")
+        write_file(recording, output_file_path)
+        transcription = transcribe(output_file_path)
+        return transcription
+    except Exception as e:
+        print(f"An error occurred during recording: {e} ")
+        return None
 
+# def write_file(recording, file_path):
+#     if recording is None:
+#         print("there is no audio file to process")
+#         return 0
+#     recording = (recording * 32767).astype('int16')
+#     wav_file = AudioSegment(recording.tobytes(), frame_rate=44100, sample_width=2, channels=1)
+#     wav_file.export(file_path, format="wav")
 def write_file(recording, file_path):
-    recording = (recording * 32767).astype('int16')
-    wav_file = AudioSegment(recording.tobytes(), frame_rate=44100, sample_width=2, channels=2)
-    wav_file.export(file_path, format="wav")
+    if recording is None or not isinstance(recording, np.ndarray):
+        print("Invalid or no audio data to process")
+        return 0
+
+    try:
+        recording = (recording * 32767).astype('int16')
+        wav_file = AudioSegment(recording.tobytes(), frame_rate=44100, sample_width=2, channels=1)  # Adjust channels if needed
+        wav_file.export(file_path, format="wav")
+        return 1
+    except Exception as e:
+        print(f"An error occurred while writing the audio file: {e}")
+        return 0
 
 def transcribe(file_path):
-    with open(file_path, "rb") as audio_file:
-        raw_transcript = client.audio.transcriptions.create(
-            model="whisper-1", 
-            file=audio_file, 
-            response_format="text"
-        )
-        transcript = raw_transcript.get('text', '')
-    return transcript
-    
+    try:
+        with open(file_path, "rb") as audio_file:
+            raw_transcript = client.audio.transcriptions.create(
+                model="whisper-1", 
+                file=audio_file, 
+                response_format="text"
+            )
+            # Directly use raw_transcript as it's already in text format
+            return raw_transcript
+    except Exception as e:
+        print(f"An error occurred during transcription: {e}")
+        return ""
+
 def translate(transcript, output_language):
     prompt = f"Translate the following text to '{output_language}': {transcript}"
     response = openai.ChatCompletion.create(
@@ -93,30 +121,46 @@ def streamed_audio(translation):
         print(f"An error occurred in audio streaming: {e}")
 
 
+def on_press(key):
+    global recording, recording_data
+    if key == keyboard.Key.enter and not recording:
+        recording_data = start_recording()  # Start recording
+        recording = True  # Set the flag to indicate recording is in progress
+
+def on_release(key):
+    global recording, recording_data
+    if key == keyboard.Key.enter and recording:
+        recording = False  # Reset the recording flag
+        if recording_data is None:
+            print("Recording failed. No data to process.")
+        else:
+            processed_data = stop_recording_and_process(recording_data)
+            try:
+                write_file(processed_data, output_file_path)
+            except Exception as e:
+                print(f"there was an error in write file on release: {e}")
+            transcription = transcribe(output_file_path)
+            translation = translate(transcription, output_language)
+            try:
+                streamed_audio(translation)
+            except Exception as e:
+                print(f"there was an error in write file on release: {e}")
+
+    if key == keyboard.Key.esc:
+        return False
+
+
 # Main interaction loop
 def main_loop():
-    print("Hold Enter while speaking, let go to stop. Press 'q' to quit.")
+    global recording, output_language
     recording = False
+    print("\n# Welcome to your AI-powered voice translator #\n")
     output_language = input("Please enter desired output language: ")
+    print("Press Enter to start and stop recording. Press 'Esc' to quit.")
 
-    while True:
-        if keyboard.is_pressed('Enter') and not recording:
-            start_recording()
-            recording = True
-            while keyboard.is_pressed('Enter'):
-                pass  # Wait for key release
-
-            # Stop recording and process the audio
-            recording_data = stop_recording_and_process()
-            write_file(recording_data, 'output.wav')
-            transcription = transcribe('output.wav')
-            translation = translate(transcription, output_language)
-            streamed_audio(translation)
-            recording = False
-
-        elif keyboard.is_pressed('q'):
-            print("\nYou have ended your session.\n")
-            break
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
 
 
 if __name__ == '__main__':
